9fa51cd983fa7fe83ede1990b8e77fb91baa6908
MatiCarrera <mcarreradev12@gmail.com>
2025-04-10 14:23:03 -0300

    manejo de excepciones

--- velyo_backend/velyo_backend/src/main/java/com/backend/velyo_backend/VelyoBackendApplication.java
+++ velyo_backend/velyo_backend/src/main/java/com/backend/velyo_backend/VelyoBackendApplication.java
@@ -1,5 +1,6 @@
 package com.backend.velyo_backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VelyoBackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+		System.setProperty("DATABASE_USER", dotenv.get("DATABASE_USER"));
+		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
 		SpringApplication.run(VelyoBackendApplication.class, args);
 	}
-
 }
